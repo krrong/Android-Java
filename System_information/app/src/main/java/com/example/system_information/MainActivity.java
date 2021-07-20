3353c666c55cc14c8cfbba61d03ebb1bf190d6b7
krrong <krrong>
2021-07-21 00:21:23 +0900

    시스템정보 수집 앱

---
+++ System_information/app/src/main/java/com/example/system_information/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.system_information;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "PhoneState";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
